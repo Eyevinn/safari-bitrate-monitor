e2671aea91a34e512a10e7ccb24d2d8ea8da08d5
Erik <erik.johansson87@gmail.com>
2022-04-25 15:00:00 +0200

    fix: changed demo content

--- demo/demo.js
+++ demo/demo.js
@@ -5,10 +5,11 @@ document.addEventListener("DOMContentLoaded", () => {
   const handler = (quality) => {
     console.log(`bitrate changed –>`, quality);
   };
-  const src = "https://lbs-usp-hls-vod.cmore.se/vod/36af2/mdbyobowjbn(13730007_ISMUSP).ism/mdbyobowjbn(13730007_ISMUSP).m3u8?hls_no_audio_only=true";
+  const src =
+    "https://f53accc45b7aded64ed8085068f31881.egress.mediapackage-vod.eu-north-1.amazonaws.com/out/v1/1c63bf88e2664639a6c293b4d055e6bb/ade303f83e8444d69b7658f988abb054/2a647c0cf9b7409598770b9f11799178/manifest.m3u8";
   new SafariBitrateMonitor({
     videoElement,
     hlsManifestUrl: src,
-    handler
+    handler,
   });
 });
